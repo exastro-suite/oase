9daf0160ad17bad8022203fb1a0fc7d60f2c74d1
fukuda-takashi <fukuda-takashi@ncontr.com>
2021-04-06 16:55:50 +0900

    #6030 (pytest)agent君修正

--- oase-root/tests/backyards/agent_driver/test_oase_agent.py
+++ oase-root/tests/backyards/agent_driver/test_oase_agent.py
@@ -35,6 +35,7 @@ from libs.commonlibs.define import *
 from libs.webcommonlibs.events_request import EventsRequestCommon
 from libs.webcommonlibs.oase_mail import OASEMailSMTP
 from web_app.models.models import EventsRequest, RuleType, System
+from web_app.models.models import ActionType, DataObject, DriverType, RhdmResponseCorrelation
 
 oase_root_dir = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '../..')
@@ -143,6 +144,7 @@ def del_test_data():
 
     RuleType.objects.filter(last_update_user='pytest').delete()
     System.objects.filter(last_update_user='pytest').delete()
+    RhdmResponseCorrelation.objects.filter(last_update_user='pytest').delete()
 
 
 class DummyDriver:
@@ -150,6 +152,8 @@ class DummyDriver:
     def __init__(self):
 
         self.ruletype = None
+        self.request_type_id = None
+        self.trace_id = None
 
 
 class DummyDMController:
@@ -162,6 +166,89 @@ class DummyDMController:
 def _dummy_send(*args, **kwargs):
 
     pass
+
+
+def non_dummy(*args, **kwargs):
+    return None
+
+
+def rhdm_dummy(*args, **kwargs):
+
+    STS_REGISTED     = 0
+    STS_REGISTABLE   = 1
+    STS_NOTACHIEVE   = 2
+    STS_ACHIEVED     = 3
+    STS_UNACHIEVABLE = 4
+    now = datetime.datetime.now(pytz.timezone('UTC'))
+
+    try:
+        with transaction.atomic():
+            # rhdmコリレーション情報
+            #rcnt = RhdmResponseCorrelation.objects.filter(
+            #    rule_type_id    = 1,
+            #    request_type_id = 1,
+            #    rule_name       = 'test_name'
+            #).count()
+            #
+            #if rcnt > 0:
+            #    RhdmResponseCorrelation.objects.filter(
+            #        rule_type_id    = 1,
+            #        request_type_id = 1,
+            #        rule_name       = 'test_name'
+            #    ).update(
+            #        cond_large_group          = 'group02',
+            #        cond_large_group_priority = 1,
+            #        cond_small_group          = 'group20',
+            #        cond_small_group_priority = 1,
+            #        cond_count                = 3,
+            #        cond_term                 = 100,
+            #        current_count             = 1,
+            #        start_time                = now,
+            #        response_detail_id        = 1,
+            #        status                    = STS_REGISTED,
+            #        last_update_timestamp     = now,
+            #        last_update_user          = 'administrator',
+            #    )
+            #
+            #else:
+            #    RhdmResponseCorrelation(
+            #        rule_type_id              = 1,
+            #        rule_name                 = 'test_name',
+            #        request_type_id           = 1,
+            #        cond_large_group          = 'group01',
+            #        cond_large_group_priority = 1,
+            #        cond_small_group          = 'group10',
+            #        cond_small_group_priority = 1,
+            #        cond_count                = 3,
+            #        cond_term                 = 100,
+            #        current_count             = 1,
+            #        start_time                = now,
+            #        response_detail_id        = 1,
+            #        status                    = STS_REGISTED,
+            #        last_update_timestamp     = now,
+            #        last_update_user          = 'administrator',
+            #    ).save(force_insert=True)
+
+            RhdmResponseCorrelation(
+                rule_type_id              = 1,
+                rule_name                 = 'test_name',
+                request_type_id           = 1,
+                cond_large_group          = 'group01',
+                cond_large_group_priority = 1,
+                cond_small_group          = 'group10',
+                cond_small_group_priority = 1,
+                cond_count                = 3,
+                cond_term                 = 100,
+                current_count             = 1,
+                start_time                = now,
+                response_detail_id        = 1,
+                status                    = STS_REGISTED,
+                last_update_timestamp     = now,
+                last_update_user          = 'administrator',
+                ).save(force_insert=True)
+
+    except Exception as e:
+        print(e)
 
 
 ################################################
@@ -419,3 +506,179 @@ def test_notify_unknown_event_ng_noparam(django_db_setup_with_system_dmsettings,
     del_test_data()
 
 
+################################################
+# テスト(コリレーション)
+################################################
+@pytest.mark.django_db
+def test_make_rhdm_response_correlation_ok(django_db_setup_with_system_dmsettings, monkeypatch):
+    """
+    ルールマッチング結果コリレーション管理に登録するためのデータを作る
+    正常系(空)
+    """
+    from backyards.agent_driver import oase_agent as ag
+    classAgent = ag.Agent()
+    classDrv = DummyDriver()
+    classDMCtrl = DummyDMController()
+
+    # テストデータ作成
+    del_test_data()
+    set_data_smtp()
+    classDrv.ruletype = set_data_ruletype()
+    classDMCtrl.driver = classDrv
+    classAgent.dmctl = classDMCtrl
+    parameterInfo = []
+    preInfo = []
+    id = '1'
+    rulename = 'test_name'
+    cond_count = '1'
+    cond_term = '100'
+    cond_group1 = 'X'
+    cond_priority1 = '1'
+    cond_group2 = 'X'
+    cond_priority2 = '1'
+
+
+    acts = {'parameterInfo':parameterInfo,
+            'preInfo':preInfo,
+            'id':id,
+            'ruleName':rulename,
+            'condCount':cond_count,
+            'condTerm':cond_term,
+            'condGroup1':cond_group1,
+            'condPriority1':cond_priority1,
+            'condGroup2':cond_group2,
+            'condPriority2':cond_priority2}
+    responseid = 1
+
+    monkeypatch.setattr(classAgent, 'check_group_cond', non_dummy)
+
+    # テスト
+    result = True
+    try:
+        classAgent.make_rhdm_response_correlation(acts, responseid)
+    except Exception as e:
+        print(traceback.format_exc())
+        result = False
+
+    # テスト結果判定
+    assert result
+
+    del_test_data()
+
+
+@pytest.mark.django_db
+def test_check_group_cond_ok_digit(django_db_setup_with_system_dmsettings, monkeypatch):
+    """
+    グループ情報を持つルールのアクション情報をチェック
+    正常系(グルーピングなし)
+    """
+    from backyards.agent_driver import oase_agent as ag
+    classAgent = ag.Agent()
+    classDrv = DummyDriver()
+    classDMCtrl = DummyDMController()
+
+    # テストデータ作成
+    del_test_data()
+    set_data_smtp()
+    classDrv.ruletype = set_data_ruletype()
+    classDMCtrl.driver = classDrv
+    classAgent.dmctl = classDMCtrl
+
+    resp_id = 1
+    rule_name = 'test name'
+    cond_cnt = 1
+    cond_term = 100
+    group1_name = 'X'
+    priority1 = 1
+    group2_name = 'X'
+    priority2 = 1
+
+    ret = classAgent.check_group_cond(
+        resp_id,
+        rule_name,
+        cond_cnt,
+        cond_term,
+        group1_name,
+        priority1,
+        group2_name,
+        priority2)
+    assert len(ret) == 0
+
+@pytest.mark.django_db
+def test_check_group_cond_ok(django_db_setup_with_system_dmsettings, monkeypatch):
+    """
+    グループ情報を持つルールのアクション情報をチェック
+    正常系(グルーピングあり)
+    """
+    from backyards.agent_driver import oase_agent as ag
+    classAgent = ag.Agent()
+    classDrv = DummyDriver()
+    classDMCtrl = DummyDMController()
+
+    # テストデータ作成
+    del_test_data()
+    set_data_smtp()
+    classDrv.ruletype = set_data_ruletype()
+    classDMCtrl.driver = classDrv
+    classAgent.dmctl = classDMCtrl
+
+    resp_id = 1
+    rule_name = 'test name'
+    cond_cnt = 1
+    cond_term = 100
+    group1_name = 'group01'
+    priority1 = 1
+    group2_name = 'group10'
+    priority2 = 1
+
+    now = datetime.datetime.now(pytz.timezone('UTC'))
+
+    del_test_data()
+    rhdm_dummy()
+
+    # テスト判定
+    ret = classAgent.check_group_cond(
+        resp_id,
+        rule_name,
+        cond_cnt,
+        cond_term,
+        group1_name,
+        priority1,
+        group2_name,
+        priority2)
+    assert len(ret) == 9
+
+    del_test_data()
+
+@pytest.mark.django_db
+def test_check_rhdm_response_correlation_ok(django_db_setup_with_system_dmsettings, monkeypatch):
+    """
+    マッチング結果コリレーション管理の状態をチェック
+    正常系()
+    """
+    from backyards.agent_driver import oase_agent as ag
+    classDrv = DummyDriver()
+    classDMCtrl = DummyDMController()
+
+    # テストデータ作成
+    del_test_data()
+    set_data_smtp()
+    classDrv.ruletype = set_data_ruletype()
+    classDMCtrl.driver = classDrv
+
+    now = datetime.datetime.now(pytz.timezone('UTC'))
+
+    rhdm_dummy()
+
+    # テスト
+    result = True
+    try:
+        ag.check_rhdm_response_correlation(now)
+    except Exception as e:
+        print(traceback.format_exc())
+        result = False
+
+    # テスト結果判定
+    assert result
+
+    del_test_data()
