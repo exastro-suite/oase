52d5b992f90f844071c76ca8d966f8a26be13470
Kou-Nakajima <nakajima.kouson@ncontr.com>
2021-11-17 17:43:49 +0900

    #1368 アクセプトの不具合改修

--- oase-root/backyards/accept_driver/oase_accept.py
+++ oase-root/backyards/accept_driver/oase_accept.py
@@ -145,7 +145,6 @@ def make_evinfo_str(json_str):
     [メソッド概要]
       DB登録用にイベント情報を文字列に整形
     """
-
     evinfo_str = ''
 
     for v in json_str[EventsRequestCommon.KEY_EVENTINFO]:
@@ -171,7 +170,7 @@ def make_evinfo_str(json_str):
 
 ################################################
 def data_list(body, user, rule_type_id_list, label_count_list):
-    """
+    """ 
     [メソッド概要]
       DB登録するデータをリストにする。
     """
@@ -222,10 +221,10 @@ def data_list(body, user, rule_type_id_list, label_count_list):
         # イベント情報のチェック
         check_evinfo_error(trace_id, json_str, ruletypeid, evinfo_length)
 
-        # DB登録用に整形
+        # DB登録用に整形 evinfo_str ⇒ バリデーションチェック  evinfo_str_db ⇒ DB登録用
         time_zone = settings.TIME_ZONE
-        evinfo_str = make_evinfo_str(json_str)
-        evinfo_str = '{"EVENT_INFO":[%s]}' % (evinfo_str)
+        evinfo_str = json.dumps(json_str[EventsRequestCommon.KEY_EVENTINFO])
+        evinfo_str = '{"EVENT_INFO":%s}' % (evinfo_str)
         event_dt = json_str[EventsRequestCommon.KEY_EVENTTIME]
         event_dt = TimeConversion.get_time_conversion_utc(
             event_dt, time_zone)
