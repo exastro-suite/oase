d0051d160fb39bdb86a0df399bf9e5e52727a153
y-koyama-git <you-koyama@ncontr.com>
2022-04-26 19:53:34 +0900

    #1975 マニュアルコミット

--- oase_documents/conf.py
+++ oase_documents/conf.py
@@ -22,7 +22,7 @@ copyright = '2019 NEC Corporation'
 author = 'NEC'
 
 # The full version, including alpha/beta/rc tags
-release = '1.5.0'
+release = '1.6.0'
 
 
 # -- General configuration ---------------------------------------------------
